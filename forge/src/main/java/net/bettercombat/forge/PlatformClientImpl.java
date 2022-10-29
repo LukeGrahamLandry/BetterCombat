5dcecd7b9f31d172d786eb72ac52afcac830901d
Luke Graham Landry <40009893+LukeGrahamLandry@users.noreply.github.com>
2022-10-29 19:00:14 -0400

    fix keybinds

--- forge/src/main/java/net/bettercombat/forge/PlatformClientImpl.java
+++ forge/src/main/java/net/bettercombat/forge/PlatformClientImpl.java
@@ -1,5 +1,6 @@
 package net.bettercombat.forge;
 
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.common.MinecraftForge;
@@ -9,7 +10,9 @@ import java.util.List;
 
 public class PlatformClientImpl {
     public static void registerKeyBindings(List<KeyBinding> keyBindings) {
-        // Do nothing, Forge asks us to register
+        for(KeyBinding keybinding: keyBindings) {
+            KeyBindingHelper.registerKeyBinding(keybinding);
+        }
     }
     public static void onEmptyLeftClick(PlayerEntity player) {
         MinecraftForge.EVENT_BUS.post(new PlayerInteractEvent.LeftClickEmpty(player));
